cf883fbf7578567e32a0616471d3ccaf257dd915
oloveluck <oloveluck@gmail.com>
2022-02-12 02:58:01 -0500

    making the api slowly

---
+++ backend/src/GlobetrotService.ts
@@ -0,0 +1,21 @@
+import { Player, Race } from "./types";
+
+export const fetchRace = (raceId : string) : Race | undefined => {
+    const dummyRace : Race = {
+        pointsOfInterest : [],
+        players : [],
+        id : raceId
+    }
+    return dummyRace;
+}
+
+export const addPlayer = (raceId : string, player : Player) : boolean => {
+    console.log(`Unimplemented! should add ${player} to ${raceId}`)
+    return true;
+}
+
+export const saveRace = (player : Race) : boolean => {
+    console.log(`Unimplemented! should add ${player}`)
+    return true;
+}
+
