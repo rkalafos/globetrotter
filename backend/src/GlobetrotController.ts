1a3162724baaf942aa1e88f780fee6cfc7367b77
Juan Martos <19martj@gmail.com>
2022-02-12 17:03:08 -0500

    Juan (#3)
    * lots of thigns broken
    
    * change to poi
    
    * updated tasks and race with location and price point

--- backend/src/GlobetrotController.ts
+++ backend/src/GlobetrotController.ts
@@ -3,29 +3,33 @@ import { saveRace } from "./GlobetrotService";
 import { v4 as uuidv4 } from 'uuid';
 import { Player, Race, Task } from "./types";
 import { exampleTasks } from "./examples";
+import {generateTask} from "./googleApi";
 
 export const getRace = (raceId : string) : Race | undefined => {
     return fetchRace(raceId);
 }
 
-export const createRace = (numTasks : number, initializedTasks : Record<number, Task>) => {
-    const tasks : Task[] = Array(numTasks);
-    Object.entries(initializedTasks).forEach(([key, value]) => {
-        tasks[parseInt(key)] = value;
-    })
-   const generatedTasks = exampleTasks.sort(() => 0.5 - Math.random()).slice(0, numTasks - Object.keys(initializedTasks).length);
+export const createRace = async (location :string, price_point: number, labels: string[]) => {
 
-    tasks.map(task => {
-        if (!task && !!generatedTasks.length) {
-            return  generatedTasks.splice(0, 1)[0];
-        } else {
-            return undefined;
-        }
-    })
+    const tasks =await Promise.all(labels.map(label => {
+        return generateTask(label, price_point, location);
+    }))
+
+   // const generatedTasks = exampleTasks.sort(() => 0.5 - Math.random()).slice(0, numTasks - Object.keys(initializedTasks).length);
+   //
+   //  tasks.map(task => {
+   //      if (!task && !!generatedTasks.length) {
+   //          return  generatedTasks.splice(0, 1)[0];
+   //      } else {
+   //          return undefined;
+   //      }
+
     const race : Race = {
         id : uuidv4(),
         tasks : tasks,
-        players : []
+        players : [],
+        price_point: price_point,
+        location: location
     }
 
     const result = saveRace(race);
---
+++ backend/src/googleApi.ts
@@ -0,0 +1,88 @@
+import {AddressType, Client, GeocodeResult, LatLngLiteral} from "@googlemaps/google-maps-services-js";
+import {Task} from "./types";
+
+const api_key = "AIzaSyBTKrskTOBf57xrGzHFc8uXAQEA_7q5U3w";
+
+const client = new Client({});
+
+interface poi {
+    // price_level: number;
+    user_rating?: number;
+    name?: string;
+    coords?: coords;
+    user_ratings_total?: number;
+    types?: AddressType[];
+    url?: string;
+    vicinity?: string;
+    price_point?: number;
+    formatted_address?: string;
+}
+
+interface coords {
+    lat?: number;
+    lng?: number;
+}
+
+
+export async function getGeocode(address: string) : Promise<LatLngLiteral>{
+    try{
+        const response= await client.geocode({
+            params: {
+                address: address,
+                key: api_key
+            }
+        })
+        console.info("Got Geocode")
+        return response.data.results[0].geometry.location;
+    }
+    catch (e) {
+        console.log(e);
+        throw e;
+    }
+}
+
+
+export async function generateTask(keyword: string, price_level: number, address: string) :Promise<Task|undefined>{
+    const myGeocode = await getGeocode(address)
+    const myPlaces = await client.textSearch({
+        params:{
+            location: {latitude: myGeocode['lat'], longitude:myGeocode['lng']},
+            query: keyword,
+            maxprice: price_level,
+            minprice: 0,
+            radius: 20000,
+            key: api_key
+        }
+    })
+    let tasks: Task[] = myPlaces.data.results.filter(r=>(!!r.vicinity && !!r.name && !!r.rating && !!r.vicinity && !!r.formatted_address)).map(r=> {
+        return {
+            description: "",
+            pointOfInterest: {
+                name: r.name,
+                vicinity: r.vicinity,
+                formatted_address: r.formatted_address,
+                types: r.types,
+            }
+        }
+
+    })
+    //let pois :poi[] = myPlaces.data.results.map(r => {
+    //    return {
+    //        name: r.name,
+    //        types: r.types,
+    //        rating: r.rating,
+    //        price_point: r.price_level,
+    //        vicinity: r.vicinity,
+    //        user_rating_total: r.user_ratings_total,
+    //        coords: {'lat':r.geometry?.location.lat, 'lng': r.geometry?.location.lng},
+    //        url: r.url,
+    //        formatted_address: r.formatted_address
+    //    }
+    //}).filter(r=>(!!r.coords && !!r.name && !!r.rating && !!r.vicinity && !!r.formatted_address));
+
+    if (tasks.length) {
+        return tasks[0];
+    } else {
+        throw new Error("No task found for keyword");
+    }
+}
