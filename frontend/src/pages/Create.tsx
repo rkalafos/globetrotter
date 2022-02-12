29921b2e17f0fea3d86ad5bb1d6e24c2bc469097
Trevor Condon <condon.t@northeastern.edu>
2022-02-12 14:52:07 -0500

    Added current location button, no functionality yet

--- frontend/src/pages/Create.tsx
+++ frontend/src/pages/Create.tsx
@@ -1,22 +1,33 @@
 import {
+    Button,
     Input, RangeSlider, RangeSliderFilledTrack, RangeSliderThumb, RangeSliderTrack, Select,
     Stack,
     VStack,
 } from '@chakra-ui/react';
 import {DefaultLayout} from "../layouts/DefaultLayout";
 import {Card} from "../components/Card";
+import {FaLocationArrow} from 'react-icons/fa';
 
 export const Create = () => {
     return (
         <DefaultLayout>
             <Card>
                 <VStack w={'full'}>
-                    <Stack w={'full'}>
+                    <Stack w={'full'} direction={'row'}>
                         <Select placeholder={"Select a city..."}>
                             <option>Boston</option>
                             <option>London</option>
                             <option>Paris</option>
                         </Select>
+                        <Button 
+                            leftIcon={<FaLocationArrow />}
+                            bg={'teal.700'}
+                            color={'white'}
+                            _hover={{ bg: 'teal.500' }}
+                            width='250px'>
+
+                            Use Current Location
+                        </Button>
                     </Stack>
                     <Stack>
 
