55caa2af1f77612e2c9bbc4208906d0b1d63f76c
benockert <55101856+benockert@users.noreply.github.com>
2022-02-12 14:51:28 -0500

    Formatted race task list and added  constants file

---
+++ frontend/src/constants.ts
@@ -0,0 +1,6 @@
+//contants file
+const constants = {
+    MIN_POPULAR_RATING: 4.0
+}
+
+export default constants
--- frontend/src/pages/Race.tsx
+++ frontend/src/pages/Race.tsx
@@ -2,6 +2,7 @@ import { Box, Text, Image, Badge, VStack } from '@chakra-ui/react';
 import { DefaultLayout } from "../layouts/DefaultLayout";
 import { Card } from "../components/Card";
 import { useEffect, useState } from "react";
+import constants from "../constants";
 
 export const Race = () => {
     type Race = import('../types/dto').Race;
@@ -23,16 +24,16 @@ export const Race = () => {
                 rating: 3.7, //
                 numberOfRatings: 145, // probably don't need
                 vicinity: "80 Rocks Road",
-                image: "images/task-placeholder.png"
+                image: "/images/task-placeholder.png"
             },
             {
                 name: "Boda Borg",
-                types: ["bar", "restaurant"], // establishment, point_of_interest
+                types: ["Escape Room", "Activity"], // establishment, point_of_interest
                 price_point: 1, // 0-5
                 rating: 4.8, // 
                 numberOfRatings: 100, // probably don't need
                 vicinity: "75 Causeway Street",
-                image: "images/task-placeholder.png"
+                image: "/images/task-placeholder.png"
             }]
         }
         setRace(race)
@@ -46,44 +47,27 @@ export const Race = () => {
             }
 
             return (
-                <Card maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
-                    <Image src={task.image} alt={task.name} />
-
-                    <Box p='6'>
+                <Card minW='100%' maxW='lg' borderWidth='1px' borderRadius='lg' overflow='hidden'>
+                    <Image align='center' boxSize='200px' objectFit='cover' src={task.image} alt={task.name} />
+                    <Box pt='4'>
                         <Box display='flex' alignItems='baseline'>
-                            {task.rating >= 4.0 &&
-                                <Badge borderRadius='full' px='2' colorScheme='purple'>
+                            <Box mt='1' fontWeight='semibold' as='h4' lineHeight='tight' isTruncated>
+                                {task.name}
+                            </Box>
+                            {task.rating >= constants.MIN_POPULAR_RATING &&
+                                <Badge borderRadius='full' px='2' colorScheme='purple' ml={task.rating >= constants.MIN_POPULAR_RATING ? '2' : '0'}>
                                     Popular
                                 </Badge>
                             }
-                            <Box
-                                color='gray.500'
-                                fontWeight='semibold'
-                                letterSpacing='wide'
-                                fontSize='xs'
-                                textTransform='uppercase'
-                                ml='2'
-                            >
+                            <Box color='gray.500' fontWeight='semibold' letterSpacing='wide' fontSize='xs' textTransform='uppercase' ml='2'>
                                 {task.types[0]} &bull; {priceString}
                             </Box>
                         </Box>
-                        <Box
-                            mt='1'
-                            fontWeight='semibold'
-                            as='h4'
-                            lineHeight='tight'
-                            isTruncated
-                        >
-                            {task.name}
-                        </Box>
-
                         <Box>
-                            {task.vicinity}
+                            <Text fontSize='sm'>{task.vicinity}</Text>
                         </Box>
-
-
                     </Box>
-                </Card>
+                </Card >
             );
         });
     };
