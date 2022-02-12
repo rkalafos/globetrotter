439b953366d2510ee7b15b7398ce1769706c452d
juan <19martj@gmail.com>
2022-02-12 17:18:24 -0500

    comment examples.ts out

--- backend/src/examples.ts
+++ backend/src/examples.ts
@@ -1,103 +1,103 @@
-import { Player, POI, Task } from "./types";
-
-export const examplePlayers: Record<string, Player> = {
-    owen : {
-        id : 0,
-        name: 'owen'
-    },
-    raj : {
-        id : 1,
-        name: 'raj'
-    },
-    rachel : {
-        id : 2,
-        name:'rachel'
-    },
-    juan : {
-        id : 3,
-        name: 'juan'
-    },
-    guac : {
-        id : 4,
-        name: 'guac'
-    },
-    ockbert : {
-        id : 5,
-        name: 'ockeberto'
-    },
-    sky : {
-        id : 6,
-        name: 'sky'
-    },
-}
-
-const examplePOIs : Record<string, POI> = {
-    commons : {
-        name : 'boston common',
-        location : '42.35631068840035, -71.06561854566786'
-    },
-    constitution : {
-        name : 'ussConstitution',
-        location : '42.37264348250323, -71.05658300333998'
-    },
-    fanuil : {
-        name : 'fanuil hall',
-        location : '42.3603803703922, -71.05507184795704'
-    },
-    cedar : {
-        name : 'cheddar',
-        location : '42.327740625664916, -71.09513713403373'
-    },
-    carter : {
-        name : 'cartier field',
-        location : '42.34009474764042, -71.0852497752624'
-    },
-    longfellow : {
-        name : 'longfellow bridge',
-        location : '42.34009474764042, -71.0852497752624'
-    },
-    naked : {
-        name : 'russian bath house',
-        location : '42.39273886127295, -71.0404133675202'
-    },
-    lake : {
-        name : 'crytal lake',
-        location : '42.83280148177733, -71.17411860153665',
-    },
-}
-
-export const exampleTasks : Task[] = [
-    {
-        pointOfInterest : examplePOIs.commons,
-        description : 'Go ice skating at the frog pond'
-    },
-    {
-        pointOfInterest : examplePOIs.constitution,
-        description : 'Climb abord the USS Constitution'
-    },
-    {
-        pointOfInterest : examplePOIs.fanuil,
-        description : 'Buy food at quincy market'
-    },
-    {
-        pointOfInterest : examplePOIs.cheddar,
-        description : 'Get huge in the basement gym. Post results to lifts album'
-    },
-    {
-        pointOfInterest : examplePOIs.carter,
-        description : 'Play spikeball on carter'
-    },
-    {
-        pointOfInterest : examplePOIs.longfellow,
-        description : 'Take a selfie at the middle of the longfellow bridge'
-    },
-    {
-        pointOfInterest : examplePOIs.naked,
-        description : 'get sweaty with the lads at the russion bath house'
-    },
-    {
-        pointOfInterest : examplePOIs.lake,
-        description : 'cycle to the lake'
-    },
-    
-]
+//import { Player, POI, Task } from "./types";
+//
+//export const examplePlayers: Record<string, Player> = {
+//    owen : {
+//        id : 0,
+//        name: 'owen'
+//    },
+//    raj : {
+//        id : 1,
+//        name: 'raj'
+//    },
+//    rachel : {
+//        id : 2,
+//        name:'rachel'
+//    },
+//    juan : {
+//        id : 3,
+//        name: 'juan'
+//    },
+//    guac : {
+//        id : 4,
+//        name: 'guac'
+//    },
+//    ockbert : {
+//        id : 5,
+//        name: 'ockeberto'
+//    },
+//    sky : {
+//        id : 6,
+//        name: 'sky'
+//    },
+//}
+//
+//const examplePOIs : Record<string, POI> = {
+//    commons : {
+//        name : 'boston common',
+//        location : '42.35631068840035, -71.06561854566786'
+//    },
+//    constitution : {
+//        name : 'ussConstitution',
+//        location : '42.37264348250323, -71.05658300333998'
+//    },
+//    fanuil : {
+//        name : 'fanuil hall',
+//        location : '42.3603803703922, -71.05507184795704'
+//    },
+//    cedar : {
+//        name : 'cheddar',
+//        location : '42.327740625664916, -71.09513713403373'
+//    },
+//    carter : {
+//        name : 'cartier field',
+//        location : '42.34009474764042, -71.0852497752624'
+//    },
+//    longfellow : {
+//        name : 'longfellow bridge',
+//        location : '42.34009474764042, -71.0852497752624'
+//    },
+//    naked : {
+//        name : 'russian bath house',
+//        location : '42.39273886127295, -71.0404133675202'
+//    },
+//    lake : {
+//        name : 'crytal lake',
+//        location : '42.83280148177733, -71.17411860153665',
+//    },
+//}
+//
+//export const exampleTasks : Task[] = [
+//    {
+//        pointOfInterest : examplePOIs.commons,
+//        description : 'Go ice skating at the frog pond'
+//    },
+//    {
+//        pointOfInterest : examplePOIs.constitution,
+//        description : 'Climb abord the USS Constitution'
+//    },
+//    {
+//        pointOfInterest : examplePOIs.fanuil,
+//        description : 'Buy food at quincy market'
+//    },
+//    {
+//        pointOfInterest : examplePOIs.cheddar,
+//        description : 'Get huge in the basement gym. Post results to lifts album'
+//    },
+//    {
+//        pointOfInterest : examplePOIs.carter,
+//        description : 'Play spikeball on carter'
+//    },
+//    {
+//        pointOfInterest : examplePOIs.longfellow,
+//        description : 'Take a selfie at the middle of the longfellow bridge'
+//    },
+//    {
+//        pointOfInterest : examplePOIs.naked,
+//        description : 'get sweaty with the lads at the russion bath house'
+//    },
+//    {
+//        pointOfInterest : examplePOIs.lake,
+//        description : 'cycle to the lake'
+//    },
+//
+//]
