7afbcd39be2eae01c97206a5710318ab80ec2c3e
benockert <55101856+benockert@users.noreply.github.com>
2022-02-12 21:46:45 -0500

    Added alert for sharing link and copy link feature

--- frontend/src/pages/Race.tsx
+++ frontend/src/pages/Race.tsx
@@ -1,13 +1,14 @@
-import { Text, VStack, Stack, Heading, Flex, Grid, GridItem, Center } from '@chakra-ui/react';
+import { Text, VStack, Stack, Heading, Flex, Grid, GridItem, Alert, AlertIcon, AlertDescription, AlertTitle, CloseButton, Icon } from '@chakra-ui/react';
 import { DefaultLayout } from "../layouts/DefaultLayout";
 import { Card } from "../components/Card";
 import { useEffect, useState } from "react";
 import { TaskCard } from "../components/TaskCard";
-import { FaUser } from 'react-icons/fa';
+import { FaUser, FaLink } from 'react-icons/fa';
 import { useRaceContext } from '../services/RaceProvider';
 import { useGetRaceQuery } from '../services/api';
 import { useParams } from 'react-router-dom';
 import { skipToken } from '@reduxjs/toolkit/dist/query/react';
+import constants from "../constants";
 type Race = import('../types/dto').Race;
 
 const test_race: Race = {
@@ -78,11 +79,13 @@ export const Race = () => {
     const { id } = useParams();
     const { currentRace: race, setCurrentRace: setRace } = useRaceContext();
     const { data, isLoading, error } = useGetRaceQuery(id && !race ? id : skipToken);
+    const [alertVisible, setAlertVisible] = useState(true);
+    const [copiedAlertVisible, setCopiedAlertVisible] = useState(false)
     // TODO: Query the actual race with the id from the url, and set the race to the object
     useEffect(() => {
-        if (!race && data) {
-            setRace(data);
-        }
+        //if (!race && data) {
+        setRace(test_race);
+        //}
     }, [data]);
 
     const raceCards = () => {
@@ -96,58 +99,74 @@ export const Race = () => {
         }} />{player.name}</Flex></GridItem>)
     }
 
-    const dashboard = () => {
+    const shareLinkAlert = () => {
+        const closeAlert = () => {
+            setAlertVisible(false)
+        }
+
         return (
-// <<<<<<< Updated upstream
-//             <Card bg="#ffffffe8" w={'full'}>
-//                 <VStack w={'full'}>
-//                     <Stack direction={'row'}>
-//                         <Heading>
-//                             {race?.id}
-//                         </Heading>
-//                     </Stack>
-//                     <Stack direction={'row'}>
-//                         <Text>
-//                             <b>Location: </b>{race?.location}
-//                         </Text>
-//                     </Stack>
-//                     <Stack direction={'row'}>
-//                         <Text>
-//                             <b>Price Point: </b>{Array((race?.price_point ?? 0) + 1).fill('$').join('')}
-//                         </Text>
-//                     </Stack>
-//                     <Stack direction={'row'} pt='20px' pb='10px'>
-//                         <VStack>
-//                             <Grid templateColumns='repeat(3, 1fr)' gap={5}>
-//                                 {peopleGridItems()}
-//                             </Grid>
-// =======
-            <Card bg="#ffffffe8" w={'full'}>
-                        <VStack w={'full'}>
-                            <Stack direction={'row'}>
-                                <Heading>
-                                    Your Amazing Race
-                                </Heading>
-                            </Stack>
-                            <Stack direction={'row'}>
-                                <Text>
-                                    <b>Location: </b> {typeof race?.location === 'string' ? race?.location : `${race?.location.lat} ${race?.location.lng}`}
-                                </Text>
-                            </Stack>
-                            <Stack direction={'row'}>
-                                <Text>
-                                    <b>Price Point: </b> {Array((race?.price_point ?? 0) + 1).fill('$').join('')}
-                                </Text>
-                            </Stack>
-                            <Stack direction={'row'} pt='20px' pb='10px'>
-                                 <VStack>
-                                     <Grid templateColumns='repeat(3, 1fr)' gap={5}>
-                                         {peopleGridItems()}
-                                    </Grid>
-                                 </VStack>
-                            </Stack>
-                        </VStack>
-            </Card>
+            <Alert status='info' variant='subtle' flexDirection='column' alignItems='center' textAlign='center'>
+                <AlertIcon />
+                <AlertTitle mr={2}>Welcome to your amazing race!</AlertTitle>
+                <AlertDescription>Click the link icon below to share this race with your friends.</AlertDescription>
+
+                <CloseButton onClick={() => closeAlert()} position='absolute' right='8px' top='8px' />
+            </Alert>
+        )
+    }
+
+    const copiedLinkAlert = () => {
+        return (
+            <Alert status='success' >
+                <AlertIcon />
+                <AlertTitle mr={2}>Link copied to clipboard!</AlertTitle>
+            </Alert>
+        )
+    }
+
+    const dashboard = () => {
+        const copyLink = () => {
+            const link = constants.SITE_DOMAIN + '/race' + race?.id
+            navigator.clipboard.writeText(link)
+
+            setCopiedAlertVisible(true)
+
+            setTimeout(() => {
+                setCopiedAlertVisible(false)
+            }, 5000)
+        }
+
+        return (
+            <>
+                {alertVisible && shareLinkAlert()}
+                {copiedAlertVisible && copiedLinkAlert()}
+                <Card bg="#ffffffe8" w={'full'}>
+                    <VStack w={'full'}>
+                        <Stack direction={'row'}>
+                            <Heading>
+                                <Flex>Your Amazing Race <Icon as={FaLink} _hover={{ cursor: 'pointer' }} onClick={() => copyLink()} w={6} h={6} style={{ marginTop: '12px', marginLeft: '12px' }} /></Flex>
+                            </Heading>
+                        </Stack>
+                        <Stack direction={'row'}>
+                            <Text>
+                                <b>Location: </b> {typeof race?.location === 'string' ? race?.location : `${race?.location.lat} ${race?.location.lng}`}
+                            </Text>
+                        </Stack>
+                        <Stack direction={'row'}>
+                            <Text>
+                                <b>Price Point: </b> {Array((race?.price_point ?? 0) + 1).fill('$').join('')}
+                            </Text>
+                        </Stack>
+                        <Stack direction={'row'} pt='20px' pb='10px'>
+                            <VStack>
+                                <Grid templateColumns='repeat(3, 1fr)' gap={5}>
+                                    {peopleGridItems()}
+                                </Grid>
+                            </VStack>
+                        </Stack>
+                    </VStack>
+                </Card>
+            </>
         )
     }
 
