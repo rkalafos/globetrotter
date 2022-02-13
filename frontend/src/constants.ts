972500c87ae6fb2cd707645d53607abfae0862a6
rajp33 <rpatel3318@gmail.com>
2022-02-12 22:14:25 -0500

    Update API KEy from SSM

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,7 +1,7 @@
 //constants file
 const constants = {
     MIN_POPULAR_RATING: 4.0,
-    SITE_DOMAIN: process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3000'
+    SITE_DOMAIN: process.env.NODE_ENV === 'production' ? 'https://globe.trotter.com.s3-website-us-east-1.amazonaws.com' : 'http://localhost:3000'
 }
 
 export default constants
